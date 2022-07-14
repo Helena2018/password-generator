0a2a7928b0ed7c48b2a596066766524973cf448e
Helena2018 <helena2020liu@gmail.com>
2022-07-14 20:21:40 +1200

    each ramdom function

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ function getRamdomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-console.log(getRamdomLower())
+function getRamdomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+function getRamdomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+}
+
+function getRamdomSymbol() {
+  const symbols = '!@#$%ˆ&*(){};<>?,./'
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+console.log(getRamdomSymbol())

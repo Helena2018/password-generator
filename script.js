e5e0362d8f8b63b66f61bfe173bd2a4e36f43e24
Helena2018 <helena2020liu@gmail.com>
2022-07-14 20:41:59 +1200

    check ramdom value

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const lowerEl = document.getElementById('lowercase')
+const upperEl = document.getElementById('uppercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const lengthEl = document.getElementById('length')
+const clipboardEl = document.getElementById('clipboard')
+const generateEl = document.getElementById('generate')
+const resultEl = document.getElementById('result')
+
+const randomeFunc = {
+  lower: getRamdomLower,
+  upper: getRamdomUpper,
+  number: getRamdomNumber,
+  symbol: getRamdomSymbol
+}
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value
+  const hasUpper = upperEl.value
+  const hasLower = lowerEl.value
+  const hasNumber = numbersEl.value
+  const hasSymbol = symbolsEl.value
+  console.log(length)
+})
+
 function getRamdomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
@@ -14,5 +39,3 @@ function getRamdomSymbol() {
   const symbols = '!@#$%ˆ&*(){};<>?,./'
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
-
-console.log(getRamdomSymbol())
